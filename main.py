fb66582131263044ad38fd6cda2d1159c7aab31e
Stephenhng <shengwei331@gmail.com>
2023-05-31 02:54:47 +0800

    LatestUpdate12

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 #FYP Project 69803
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.accordion import Accordion, AccordionItem
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.popup import Popup
 from kivy.lang import Builder
+from kivy.clock import Clock
 from kivy.uix.screenmanager import ScreenManager
-from kivy.properties import StringProperty, NumericProperty, ObjectProperty
+from kivy.properties import StringProperty, NumericProperty, ObjectProperty, ListProperty
 from datetime import datetime
 import csv
 import warnings
@@ -31,8 +35,6 @@ db_path = os.path.join(BASE_DIR, "disease.db")
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-nltk.download('punkt')
-nltk.download('wordnet')
 
 lemmatizer = WordNetLemmatizer()
 intents = json.loads(open('intents.json').read())
@@ -84,6 +86,34 @@ class Label(Label):
         return super().on_touch_down(touch)
 
 
+class CircularProgressBar(AnchorLayout):
+    set_value = NumericProperty(0)
+    value = NumericProperty(0)
+    bar_color = ListProperty([1, 0, 100/255])
+    bar_width = NumericProperty(10)
+    text = StringProperty("0%")
+    duration = NumericProperty(2)
+    counter = 0
+
+    def __init__(self, **kwargs):
+        super(CircularProgressBar, self).__init__(**kwargs)
+        Clock.schedule_once(self.animate, 0)
+
+    def animate(self, *args):
+        Clock.schedule_interval(self.percent_counter, self.duration/self.value)
+
+
+    def percent_counter(self, *args):
+        if self.counter < self.value:
+            self.counter += 1
+            self.text = f"LOADING {self.counter}%"
+            self.set_value = self.counter
+            if self.counter == 100:
+                screen_manager.current = "main"
+        else:
+            Clock.unschedule(self.percent_counter)
+
+
 class parentApp(App):
 
     passwd = ObjectProperty(None)
@@ -97,6 +127,7 @@ class parentApp(App):
         Window.clearcolor = (1, 1, 1, 1)
         global screen_manager, description_list
         screen_manager = ScreenManager()
+        screen_manager.add_widget(Builder.load_file("splash.kv"))
         screen_manager.add_widget(Builder.load_file("parent.kv"))
         screen_manager.add_widget(Builder.load_file("register.kv"))
         screen_manager.add_widget(Builder.load_file("content.kv"))
@@ -776,47 +807,53 @@ class parentApp(App):
 
 
                 except Exception:
-                    symptom_list = ["itching", "skin rash", "nodal skin eruptions", "continuous sneezing", "shivering",
-                                    "chills",
-                                    "joint pain", "stomach pain", "acidity", "ulcers on tongue", "muscle wasting",
-                                    "vomiting",
-                                    "burning micturition", "spotting urination", "fatigue", "weight gain", "anxiety",
-                                    "cold hands and feets", "mood swings", "weight loss", "restlessness", "lethargy",
-                                    "patches in throat", "irregular sugar level", "cough", "high fever", "sunken eyes",
-                                    "breathlessness", "sweating", "dehydration", "indigestion",
-                                    "headache", "yellowish skin", "dark urine", "nausea", "loss of appetite",
-                                    "pain behind the eyes", "back pain", "constipation", "abdominal pain", "diarrhoea",
-                                    "mild fever", "yellow urine", "yellowing of eyes", "acute liver failure",
-                                    "fluid overload",
-                                    "swelling of stomach", "swelled lymph nodes", "malaise", "blurred and distorted vision",
-                                    "phlegm", "throat irritation", "redness of eyes", "sinus pressure", "runny nose",
-                                    "congestion",
-                                    "chest pain", "weakness in limbs", "fast heart rate", "pain during bowel movements",
-                                    "pain in anal region", "bloody stool", "irritation in anus", "neck pain", "dizziness",
-                                    "cramps",
-                                    "bruising", "obesity", "swollen legs", "swollen blood vessels", "puffy face and eyes",
-                                    "enlarged thyroid", "brittle nails", "swollen extremeties", "excessive hunger",
-                                    "extra marital contacts", "drying and tingling lips", "slurred speech", "knee pain",
-                                    "hip joint pain", "muscle weakness", "stiff neck", "swelling joints",
-                                    "movement stiffness",
-                                    "spinning movements", "loss of balance", "unsteadiness", "weakness of one body side",
-                                    "loss of smell", "bladder discomfort", "foul smell ofurine", "continuous feel of urine",
-                                    "passage of gases", "internal itching", "toxic look (typhos)", "depression",
-                                    "irritability",
-                                    "muscle pain", "altered sensorium", "red spots over body", "belly pain",
-                                    "abnormal menstruation", "dischromic patches", "watering from eyes",
-                                    "increased appetite",
-                                    "polyuria", "family history", "mucoid sputum", "rusty sputum", "lack of concentration",
-                                    "visual disturbances", "receiving blood transfusion", "receiving unsterile injections",
-                                    "coma", "stomach bleeding", "distention of abdomen", "history of alcohol consumption",
-                                    "blood in sputum", "prominent veins on calf", "palpitations", "painful walking",
-                                    "pus filled pimples", "blackheads", "scurring", "skin peeling", "silver like dusting",
-                                    "small dents in nails", "inflammatory nails", "blister", "red sore around nose",
-                                    "yellow crust ooze"]
-                    symptom_str = ",".join(symptom_list)
-                    screen_manager.get_screen('content').chat_list.add_widget(Response(text=str(symptom_str), size_hint=(.80, None)))
-                    screen_manager.get_screen('content').chat_list.add_widget(Response(text="Use the given relevant symptom as above provided without space between comma only for prediction. Eg: Itching (Not itchy/itchings)", size_hint=(.80, None)))
-
+                    # symptom_list = ["itching", "skin rash", "nodal skin eruptions", "continuous sneezing", "shivering",
+                    #                 "chills",
+                    #                 "joint pain", "stomach pain", "acidity", "ulcers on tongue", "muscle wasting",
+                    #                 "vomiting",
+                    #                 "burning micturition", "spotting urination", "fatigue", "weight gain", "anxiety",
+                    #                 "cold hands and feets", "mood swings", "weight loss", "restlessness", "lethargy",
+                    #                 "patches in throat", "irregular sugar level", "cough", "high fever", "sunken eyes",
+                    #                 "breathlessness", "sweating", "dehydration", "indigestion",
+                    #                 "headache", "yellowish skin", "dark urine", "nausea", "loss of appetite",
+                    #                 "pain behind the eyes", "back pain", "constipation", "abdominal pain", "diarrhoea",
+                    #                 "mild fever", "yellow urine", "yellowing of eyes", "acute liver failure",
+                    #                 "fluid overload",
+                    #                 "swelling of stomach", "swelled lymph nodes", "malaise", "blurred and distorted vision",
+                    #                 "phlegm", "throat irritation", "redness of eyes", "sinus pressure", "runny nose",
+                    #                 "congestion",
+                    #                 "chest pain", "weakness in limbs", "fast heart rate", "pain during bowel movements",
+                    #                 "pain in anal region", "bloody stool", "irritation in anus", "neck pain", "dizziness",
+                    #                 "cramps",
+                    #                 "bruising", "obesity", "swollen legs", "swollen blood vessels", "puffy face and eyes",
+                    #                 "enlarged thyroid", "brittle nails", "swollen extremeties", "excessive hunger",
+                    #                 "extra marital contacts", "drying and tingling lips", "slurred speech", "knee pain",
+                    #                 "hip joint pain", "muscle weakness", "stiff neck", "swelling joints",
+                    #                 "movement stiffness",
+                    #                 "spinning movements", "loss of balance", "unsteadiness", "weakness of one body side",
+                    #                 "loss of smell", "bladder discomfort", "foul smell ofurine", "continuous feel of urine",
+                    #                 "passage of gases", "internal itching", "toxic look (typhos)", "depression",
+                    #                 "irritability",
+                    #                 "muscle pain", "altered sensorium", "red spots over body", "belly pain",
+                    #                 "abnormal menstruation", "dischromic patches", "watering from eyes",
+                    #                 "increased appetite",
+                    #                 "polyuria", "family history", "mucoid sputum", "rusty sputum", "lack of concentration",
+                    #                 "visual disturbances", "receiving blood transfusion", "receiving unsterile injections",
+                    #                 "coma", "stomach bleeding", "distention of abdomen", "history of alcohol consumption",
+                    #                 "blood in sputum", "prominent veins on calf", "palpitations", "painful walking",
+                    #                 "pus filled pimples", "blackheads", "scurring", "skin peeling", "silver like dusting",
+                    #                 "small dents in nails", "inflammatory nails", "blister", "red sore around nose",
+                    #                 "yellow crust ooze"]
+                    # symptom_str = ",".join(symptom_list)
+                    # screen_manager.get_screen('content').chat_list.add_widget(Response(text=str(symptom_str), size_hint=(.85, None)))
+                    screen_manager.get_screen('content').chat_list.add_widget(Response(text="Use the given relevant symptom without space between comma only for prediction.", size_hint=(.80, None)))
+                    screen_manager.get_screen('content').chat_list.add_widget(
+                        Response(text="For Example: muscle weakness,stiff neck,swelling join,movement stiffness",
+                                 size_hint=(.80, None)))
+                    url = "https://drive.google.com/file/d/1f7WC3-_GNjdxaB8WDuPQVrq5xdvwUpV2/view?usp=drive_link"
+                    label = Label(text="See More Symptom", size_hint=(.45, None), color=(0, 0, 1, 1), underline=True)
+                    label.url = url
+                    screen_manager.get_screen('content').chat_list.add_widget(label)
 
             elif tag == 'datetime':
                 screen_manager.get_screen('content').chat_list.add_widget(Response(text=now.strftime("%A \n%d %B %Y \n%H:%M:%S"), size_hint=(.65, None)))
@@ -825,13 +862,11 @@ class parentApp(App):
             elif tag == 'goodbye':
                 screen_manager.get_screen('content').chat_list.add_widget(Response(text=res, size_hint=(.65, None)))
                 url = "https://docs.google.com/forms/d/e/1FAIpQLScpxmn2ZjA4YlngPctl9sSXLyOReiJmf28nNj-RgGjgSusEgg/viewform?usp=sf_link"
-                label = Label(text="Click here to give us feedback", size_hint=(.65, None), color=(0, 0, 1, 1), underline=True)
+                label = Label(text="Click here to give us feedback", size_hint=(.45, None), color=(0, 0, 1, 1), underline=True)
                 label.url = url
                 screen_manager.get_screen('content').chat_list.add_widget(label)
             else:
                 screen_manager.get_screen('content').chat_list.add_widget(Response(text=res, size_hint=(.65, None)))
-
-
 
             screen_manager.get_screen('content').text_input.text = ""
 
